7a2a67f649de79e27d034259421c97ed901232da
Leandro Lopes <leandro.lopes@confitec.com.br>
2018-10-24 18:42:12 -0300

    Repository Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'mac-root',
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  title = 'mac';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+
+@NgModule({
+  declarations: [AppComponent, TaskListComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
